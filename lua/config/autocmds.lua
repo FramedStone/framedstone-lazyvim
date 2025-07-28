0ee29548ce8b42879ac7a3cd6cef87f23e536296
vanoss22 <leeweixuan39@gmail.com>
2025-07-28 13:17:58 +0800

    refactor: ask for REPOSITORY_PATH on every startup

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -23,27 +23,16 @@ vim.api.nvim_create_autocmd("FileType", {
   end,
 })
 
--- MCPHub: Set REPOSITORY_PATH if not already set
+-- MCPHub: git tools
 vim.schedule(function()
   local notify = require("lazyvim.util").notify
 
-  if not vim.env.REPOSITORY_PATH or vim.env.REPOSITORY_PATH == "" then
-    local input = vim.fn.input("Set REPOSITORY_PATH (optional): ", "", "dir")
-    if input and input ~= "" then
-      vim.env.REPOSITORY_PATH = input
-      notify({
-        msg = "REPOSITORY_PATH set to:\n" .. input,
-        level = "info",
-      })
-    else
-      notify({
-        msg = "REPOSITORY_PATH not set (left blank)",
-        level = "warn",
-      })
-    end
-  else
+  local defaultPath = vim.fn.getcwd()
+  local input = vim.fn.input("Set REPOSITORY_PATH (default=CURRENT_PATH): ", defaultPath, "dir")
+  if input and input ~= "" then
+    vim.env.REPOSITORY_PATH = input
     notify({
-      msg = "REPOSITORY_PATH is already set:\n" .. vim.env.REPOSITORY_PATH,
+      msg = "REPOSITORY_PATH set to:\n" .. input,
       level = "info",
     })
   end
