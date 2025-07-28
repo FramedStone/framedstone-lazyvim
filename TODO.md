725462548434afc9d456665fcd43899c93713c76
vanoss22 <leeweixuan39@gmail.com>
2025-07-28 12:32:10 +0800

    chore: point Todo cmd to always open TODO.md from nvim config folder

--- TODO.md
+++
@@ -1,4 +0,0 @@
-### Last modified: 2025-07-28 12::16::59
-
-- [ ] lazyvim laptop
-- [ ] continue onboarding page
--- lua/config/cmds.lua
+++ lua/config/cmds.lua
@@ -1,13 +1,4 @@
 vim.api.nvim_create_user_command("Todo", function()
-  local filePath = "TODO.md"
+  local filePath = vim.fn.stdpath("config") .. "/TODO.md"
   vim.cmd("edit " .. filePath)
-
-  vim.api.nvim_create_autocmd("BufEnter", {
-    once = true,
-    pattern = filePath,
-    callback = function()
-      local now = os.date("### Last modified: %Y-%m-%d %H::%M::%S")
-      vim.api.nvim_buf_set_lines(0, 0, 1, false, { now }) -- replace first line
-    end,
-  })
-end, { desc = "Open TODO.md file and update timestamp" })
+end, { desc = "Open TODO.md in Neovim config " })
