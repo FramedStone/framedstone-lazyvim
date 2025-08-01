0f971adbd97979d0a7262b08cc7aff19d36cd15b
vanoss22 <leeweixuan39@gmail.com>
2025-08-01 18:36:23 +0800

    refactor(plugin): updated mcphub config with copilotchat

--- lua/plugins/mcphub.lua
+++ lua/plugins/mcphub.lua
@@ -117,9 +117,15 @@ return {
       end
 
       require("mcphub").setup({
-        figmaToken = figmaToken,
+        extensions = {
+          copilotchat = {
+            enabled = true,
+            convert_tools_to_functions = true, -- Convert MCP tools to CopilotChat functions
+            convert_resources_to_functions = true, -- Convert MCP resources to CopilotChat functions
+            add_mcp_prefix = false, -- Add "mcp_" prefix to function names
+          },
+        },
       })
-
       local chat = require("CopilotChat")
       local mcp = require("mcphub")
 
