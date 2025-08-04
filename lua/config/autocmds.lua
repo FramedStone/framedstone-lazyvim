4d9d76acc4825ad33055d8c7cf83e4169c206774
vanoss22 <leeweixuan39@gmail.com>
2025-08-04 11:06:09 +0800

    feat: fileformat set as unix

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -6,15 +6,15 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
-vim.api.nvim_create_autocmd("BufReadPost", {
-  pattern = "*",
-  callback = function()
-    if vim.bo.fileformat == "dos" then
-      vim.bo.fileformat = "unix"
-      vim.notify("Line endings converted: CRLF → LF", vim.log.levels.INFO)
-    end
-  end,
-})
+-- vim.api.nvim_create_autocmd("BufReadPost", {
+--   pattern = "*",
+--   callback = function()
+--     if vim.bo.fileformat == "dos" then
+--       vim.bo.fileformat = "unix"
+--       vim.notify("Line endings converted: CRLF → LF", vim.log.levels.INFO)
+--     end
+--   end,
+-- })
 
 vim.api.nvim_create_autocmd("FileType", {
   pattern = "*",
