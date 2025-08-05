8a2ad78c52d362fb907a8ad4b70c16dfd6ac7724
vanoss22 <leeweixuan39@gmail.com>
2025-08-06 06:04:31 +0800

    feat: avante + blink modification for autocompletion

--- lua/plugins/disable.lua
+++ lua/plugins/disable.lua
@@ -1,3 +1,6 @@
 return {
   { "folke/todo-comments.nvim", enabled = false },
+  -- { "CopilotC-Nvim/CopilotChat.nvim", enabled = false },
+  -- { "ravitemer/mcphub.nvim", enabled = false },
+  { "yetone/avante.nvim", enabled = false },
 }
