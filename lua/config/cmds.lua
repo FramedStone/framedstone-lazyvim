d785f8d39e30a478ef16c6022ebcb2ae4ef47ee5
vanoss22 <leeweixuan39@gmail.com>
2025-08-19 00:44:16 +0800

    feat: config cmd

--- lua/config/cmds.lua
+++ lua/config/cmds.lua
@@ -2,3 +2,8 @@ vim.api.nvim_create_user_command("Obsidian", function()
   local indexPath = vim.fn.expand("~/.config/obsidian-sync/README.md")
   vim.cmd("edit " .. indexPath)
 end, { desc = "Open Obsidian README.md in Neovim" })
+
+vim.api.nvim_create_user_command("Config", function()
+  local indexPath = vim.fn.expand("~/.config/nvim/init.lua")
+  vim.cmd("edit " .. indexPath)
+end, { desc = "Open Obsidian config file (init.lua) in Neovim" })
