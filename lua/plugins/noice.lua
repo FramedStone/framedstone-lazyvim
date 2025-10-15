9645497817317616af5341a3145302cc21ca43a7
vanoss222 <leeweixuan39@gmail.com>
2025-10-15 15:29:18 +0800

    chore: change file name

---
+++ lua/plugins/noice.lua
@@ -0,0 +1,20 @@
+return {
+  "folke/noice.nvim",
+  opts = {
+    views = {
+      cmdline_popup = {
+        position = {
+          row = "50%", -- vertical center
+          col = "50%", -- horizontal center
+        },
+        size = {
+          width = "auto",
+          height = "auto",
+        },
+        border = {
+          style = "rounded",
+        },
+      },
+    },
+  },
+}
